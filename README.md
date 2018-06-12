203533953d9bd6c73b642fc33607532f31bb3d6b
Tiago Fuelber <tiago.fuelber@cwi.com.br>
2018-06-12 18:54:05 -0300

    Adds babel-loader

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 ## Install dependencies
 `npm i --save-dev babel-core webpack`
+
+* Babel loader
+It's the middleware between webpack and babel
+`npm i --save-dev babel-loader`
+
+
+## webpack.config options
+`entry`
+Apps entry point
+
+`output`
+Apps output path
+
+`module`
+Contains the rules for the loaders
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                //all file extensions webpack will consider
+                test: /\.js$/,
+                //webpack will exclude
+                exclude: /node_modules/,
+                //which loader it will use
+                use: 'babel-loader'
+            }
+        ]
     }
 }
